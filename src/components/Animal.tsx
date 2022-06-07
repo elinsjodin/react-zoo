9e3cb8448e01d3f32fe2df7fecf96bf438eb615d
elinsjodin <elinsjodin@outlook.com>
2022-06-07 15:37:22 +0200

    Implemented fetch from API and save response data to local storage

--- src/components/Animal.tsx
+++ src/components/Animal.tsx
@@ -1,3 +1,31 @@
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { IAnimal } from "../models/IAnimal";
+
 export const Animal = () => {
-  return <></>;
+  const [animal, setAnimal] = useState<IAnimal>({
+    name: "",
+    latinName: "",
+    imageUrl: "",
+    longDescription: "",
+  });
+
+  let params = useParams();
+
+  useEffect(() => {
+    fetch("https://animals.azurewebsites.net/api/animals/" + params.id)
+      .then((response) => response.json())
+      .then((singleAnimalData) => setAnimal(singleAnimalData));
+  });
+
+  return (
+    <>
+      <Link to="/">Back to Home Page</Link>
+      <h3>
+        {animal.name} ({animal.latinName})
+      </h3>
+      <img src={animal.imageUrl} alt={animal.name} />
+      Decription: {animal.longDescription}
+    </>
+  );
 };
--- src/components/ShowAnimals.tsx
+++ src/components/ShowAnimals.tsx
@@ -1,3 +1,54 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { IAnimal } from "../models/IAnimal";
+
 export const ShowAnimals = () => {
-  return <></>;
+  const [animals, setAnimals] = useState<IAnimal[]>([]);
+  // const [animalFed, setAnimalFed] = useState(false);
+
+  const LOCAL_STORAGE_LIST_KEY = "animals";
+
+  JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY) || "[]");
+
+  const saveToLocalStorage = (animalList: IAnimal[]) => {
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(animalList));
+  };
+
+  useEffect(() => {
+    if (animals.length !== 0) return;
+
+    fetch("https://animals.azurewebsites.net/api/animals")
+      .then((response) => response.json())
+      .then((animalData) => {
+        saveToLocalStorage(animalData);
+        setAnimals(animalData);
+      });
+  });
+
+  // const handleAnimalFed = () => {
+  //   if (animalFed){
+
+  //   }
+
+  //   saveToLocalStorage()
+  // }
+
+  return (
+    <>
+      {animals.map((animal) => {
+        return (
+          <div key={animal.id}>
+            <Link to={"/animal/" + animal.id}>
+              <h3>{animal.name}</h3>
+              <p></p>
+              <div>
+                <img src={animal.imageUrl} alt={animal.name}></img>
+              </div>
+            </Link>
+            {/* <button onClick={handleAnimalFed}>Mata djur</button> */}
+          </div>
+        );
+      })}
+    </>
+  );
 };
